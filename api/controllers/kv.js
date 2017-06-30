2b38bba25de3a950fe1d823d5dbf297c8126fe7c
nick.li <nick.li@nicklide-MacBook-Pro.local>
2017-06-30 17:21:55 +0800

    modify task 1, using object to store data instead of using a file

--- api/controllers/kv.js
+++ api/controllers/kv.js
@@ -1,28 +1,24 @@
 const fs = require('fs');
 const URLSafeBase64 = require('urlsafe-base64');
+var store = {};
 
 function getKEY(req, res) {
   var key = req.swagger.params.KvKey.value;
 
   //key check
   if(URLSafeBase64.validate(key) && key){
-    fs.readFile('store.dat', (err, data)=>{
-      if(err) throw err;
-      
-      var store = JSON.parse(data);
-      if(store[key]){
-        let now =(new Date()).toISOString();
-        res.json(200, {
-          VALUE: store[key],
-          TS: now
-        });
-      }
-      else{
-        res.json(404, {
-          message: 'Key is not found',
-        });
-      }
-    });
+    if(store[key]){
+      let now =(new Date()).toISOString();
+      res.json(200, {
+        VALUE: store[key],
+        TS: now
+      });
+    }
+    else{
+      res.json(404, {
+        message: 'Key is not found',
+      });
+    }
   }
   else{
     res.json(400, {
@@ -36,35 +32,15 @@ function deleteKEY(req, res) {
 
   //key check
   if(URLSafeBase64.validate(key) && key){
-    fs.readFile('store.dat', (err, data)=>{
-      if(err) throw err;
-      
-      var store = JSON.parse(data);
-      var resObj = {}
-      var now = (new Date()).toISOString();
-      
-      if(store[key]){
-        resObj.OLD_VALUE = store[key];
-        resObj.TS = now;
-        
-        delete store[key];
-        fs.writeFile('store.dat', JSON.stringify(store), (err)=>{
-          if(err){
-            res.json(500, {
-              message: 'Failed to write data.'
-            });
-            throw err;
-          }
-          else{
-            res.json(200, resObj);
-          }
-        });
-      }
-      else{
-        resObj.TS = now;
-        res.json(200, resObj);
-      }
-    });
+    var resObj = {}
+    var now = (new Date()).toISOString();
+    
+    if(store[key]){
+      resObj.OLD_VALUE = store[key];
+      delete store[key];
+    }
+    resObj.TS = now;
+    res.json(200, resObj);
   }
   else{
     res.json(400, {
@@ -79,19 +55,10 @@ function postKEY(req, res) {
 
   //key check
   if(URLSafeBase64.validate(key) && key){
-    fs.readFile('store.dat', (err, data)=>{
-    if(err) throw err;
-
-    var store = JSON.parse(data);
+    let now = (new Date()).toISOString();
     store[key] = val;
-
-    fs.writeFile('store.dat', JSON.stringify(store), (err)=>{
-        if(err) throw err;
-        let now = (new Date()).toISOString();
-        res.json(200, {
-          TS: now,
-        });
-      });
+    res.json(200, {
+      TS: now,
     });
   }
   else{
